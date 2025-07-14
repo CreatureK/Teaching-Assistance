c644e2c8251bf66aca5c2b87f849467e238bb45b
jz35 <14602645+zmuster@user.noreply.gitee.com>
2025-07-14 19:46:49 +0800

    feat(backend): initialize springboot + mybatis framework

---
+++ backend/src/main/java/com/java_web/backend/Mapper/SyllabusMapper.java
@@ -0,0 +1,7 @@
+package com.java_web.backend.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.java_web.backend.Entity.Syllabus;
+
+public interface SyllabusMapper extends BaseMapper<Syllabus> {
+}
